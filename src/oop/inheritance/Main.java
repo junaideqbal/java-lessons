90621fa5efb90b311ff3f89126f09303c53b1b99
junaid <junaid@192.168.174.1>
2017-11-29 14:35:33 +0500

    Understanding Inheritance

---
+++ src/oop/inheritance/Main.java
@@ -0,0 +1,9 @@
+package oop.inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MountainBike myBike = new MountainBike();
+	}
+
+}
---
+++ src/oop/inheritance/MountainBike.java
@@ -0,0 +1,25 @@
+package oop.inheritance;
+
+public class MountainBike extends Bicycle{
+
+	// the MountainBike subclass adds one field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(int startHeight,
+                        int startCadence,
+                        int startSpeed,
+                        int startGear) {
+        super(startCadence, startSpeed, startGear);
+        seatHeight = startHeight;
+    }   
+        
+    public MountainBike() {
+    	super();
+	}
+
+	// the MountainBike subclass adds one method
+    public void setHeight(int newValue) {
+        seatHeight = newValue;
+    }
+}
